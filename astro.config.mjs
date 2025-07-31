2444d56a6f4a95184860c250594d5d511b6c251b
Thomas Cole <thomas.patrick.cole@gmail.com>
2025-07-31 09:05:29 -0400

    Update site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://thomaspcole.github.io',
+  site:'https://thomaspcole.com',
   vite: {
     plugins: [tailwindcss()]
   }
